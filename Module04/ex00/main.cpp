0de30f6033c7e4f7943c3455f591758307af5069
Robijn Van Houts <robijnvanhouts@Robijns-MacBook-Pro.local>
2020-08-06 10:20:31 +0200

    all

---
+++ Module04/ex00/main.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: robijnvanhouts <robijnvanhouts@student.      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/08/05 14:13:01 by robijnvanho   #+#    #+#                 */
+/*   Updated: 2020/08/05 18:35:55 by robijnvanho   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Sorcerer.hpp"
+
+int     main(void)
+{
+    Sorcerer one("Frans", "Human");
+    Sorcerer two;
+    return (0);
+} 
